21681f62c017bcc43b56bdc8c0a459d8ccf1c1f7
23lopez0786 <120128839+23lopez0786@users.noreply.github.com>
2022-12-19 13:30:39 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -21,3 +21,19 @@ animal.Add(Tigerr);
 
 
 Console.WriteLine(animal.Count);
+
+/*
+ * Program Name: Animal List
+ * Programmer: Seth Lopez
+ * Date: 12/19/2022
+ *
+ * Description:The way this code works is by creating a list you can then create objects, give
+those objects names and give them the attributes like the name color and breed
+after doing this you will want to add this object to the list and you do this 
+by using .Add. 
+So the way the C# code works is by creating the strings name color and breed
+and making them equal to the paramaters n, c, b and then you derive the Animal
+class by using the :. After doing this you are able to pass these parameters to the derivied 
+classes and use them in the Cat and Tiger classes.
+
+ */
