c22ef3283333cb2c7c97557a6fbc47aa566b7958
23lopez0786 <120128839+23lopez0786@users.noreply.github.com>
2022-12-19 13:24:31 -0500

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Animal
+{
+    public string name;
+    public string color;
+    public string breed;
+
+    public Animal()
+    {
+    }
+
+    public Animal(string n, string c, string b)
+    {
+        this.name = n;
+        this.color = c;
+        this.breed = b;
+    }
+}
+
+public class cat : Animal
+{
+    public cat()
+    {
+    }
+
+    public cat(string n, string c, string b) : base(n, c, b)
+    {
+        name = n;
+        color = c;
+        breed = b;
+    }
+}
+
+public class tiger : Animal
+{
+    public tiger()
+    {
+    }
+
+    public tiger(string n, string c, string b) : base(n, c, b)
+    {
+        name = n;
+        color = c;
+        breed = b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿List<Animal> animal = new List<Animal>();
+
+
+Animal Dogg = new Animal();
+Dogg.name = "jeff";
+Dogg.color = "red";
+Dogg.breed = "husky";
+animal.Add(Dogg);
+
+cat Catt = new cat();
+Catt.name = "hen";
+Catt.color = "brown";
+Catt.breed = "garfeild";
+animal.Add(Catt);
+
+tiger Tigerr = new tiger();
+Tigerr.name = "Gary";
+Tigerr.color = "White";
+Tigerr.breed = "Snow tiger";
+animal.Add(Tigerr);
+
+
+Console.WriteLine(animal.Count);
